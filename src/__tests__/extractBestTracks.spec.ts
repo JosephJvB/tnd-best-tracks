b3d3a68e8ab71d2677f07332c0237a17cc647ee9
Joe van Bohemen <joevanbo@protonmail.com>
2023-10-26 12:06:02 +0100

    save in diff format

--- src/__tests__/extractBestTracks.spec.ts
+++ src/__tests__/extractBestTracks.spec.ts
@@ -119,20 +119,76 @@ describe('extractBestTracks.ts', () => {
 
       expect(result.length).toBeGreaterThan(0)
       expect(result).toEqual([
-        'Yard Act vs. Mad Professor - Pour More',
-        'Chat Pile - Tenkiller',
-        'Gucci Mane - Letter to Takeoff',
-        'Phoebe Bridgers - So Much Wine',
-        "Ab-Soul - Gang'Nem ft. Fre$h",
-        "Saweetie - DON'T SAY NOTHIN'",
-        'Black Belt Eagle Scout - My Blood Runs Through This Land',
-        'Fousheé - Spend the Money ft. Lil Uzi Vert',
-        'Christian Lee Hutson - Silent Night',
-        'Jane Remover - Contingency Song',
-        'Fatoumata Diawara - Nsera ft. Daman Albarn',
-        'Shame - Fingers of Steel',
-        'Kelela - On the Run',
-        'Tkay Maidza - Nights in December',
+        {
+          artist: 'Pour More',
+          link: 'https://youtu.be/Pc2dZbz87-8',
+          track: 'Yard Act vs. Mad Professor',
+        },
+        {
+          artist: 'Tenkiller',
+          link: 'https://youtu.be/U-5FKTNFz1E',
+          track: 'Chat Pile',
+        },
+        {
+          artist: 'Letter to Takeoff',
+          link: 'https://www.youtube.com/watch?v=n-9F1B_OPAM',
+          track: 'Gucci Mane',
+        },
+        {
+          artist: 'So Much Wine',
+          link: 'https://www.youtube.com/watch?v=4SBhPYhI-XA',
+          track: 'Phoebe Bridgers',
+        },
+        {
+          artist: "Gang'Nem ft. Fre$h",
+          link: 'https://youtu.be/605sWoD5qxU',
+          track: 'Ab-Soul',
+        },
+        {
+          artist: "DON'T SAY NOTHIN'",
+          link: 'https://www.youtube.com/watch?v=hDgG0_CK_jU',
+          track: 'Saweetie',
+        },
+        {
+          artist: 'My Blood Runs Through This Land',
+          link: 'https://www.youtube.com/watch?v=ntg4az1AsdM',
+          track: 'Black Belt Eagle Scout',
+        },
+        {
+          artist: 'Spend the Money ft. Lil Uzi Vert',
+          link: 'https://youtu.be/ybiy_2NQ9ik',
+          track: 'Fousheé',
+        },
+        {
+          artist: 'Silent Night',
+          link: 'https://youtu.be/RHWAlvhiAKY',
+          track: 'Christian Lee Hutson',
+        },
+        {
+          artist: 'Contingency Song',
+          link: 'https://youtu.be/cLM7xB3UDKc',
+          track: 'Jane Remover',
+        },
+        {
+          artist: 'Nsera ft. Daman Albarn',
+          link: 'https://youtu.be/VfMDqUSgbck',
+          track: 'Fatoumata Diawara',
+        },
+        {
+          artist: 'Fingers of Steel',
+          link: 'https://youtu.be/ULLsuL0y-Fk',
+          track: 'Shame',
+        },
+        {
+          artist: 'On the Run',
+          link: 'https://youtu.be/getdc1zzvnc',
+          track: 'Kelela',
+        },
+        {
+          artist: 'Nights in December',
+          link: 'https://youtu.be/2d4FTBml1Pw',
+          track: 'Tkay Maidza',
+        },
       ])
     })
 
@@ -248,14 +304,46 @@ describe('extractBestTracks.ts', () => {
       expect(result.length).toBeGreaterThan(0)
       expect(result.length).toBe(8)
       expect(result).toEqual([
-        'Chris Farren - "Say U Want Me"',
-        'Screaming Females - “Skeleton”',
-        'Denzel Curry - "Today" ft. Boogie & Allan Kingdom',
-        'A. G. Cook - "Superstar"',
-        'Dabbla - "Randeer"',
-        'Skrillex & Rick Ross - "Purple Lamborghini"',
-        'Street Sects - "And I Grew into Ribbons"',
-        'DJ Windows 98 (Win Butler) - “K33p Ur Dr34ms” (Suicide Remix)',
+        {
+          artist: '"Say U Want Me"',
+          link: 'http://www.theneedledrop.com/articles/2016/7/chris-farren-say-u-want-me',
+          track: 'Chris Farren',
+        },
+        {
+          artist: '“Skeleton”',
+          link: 'http://www.theneedledrop.com/articles/2016/7/screaming-females-skeleton',
+          track: 'Screaming Females',
+        },
+        {
+          artist: '"Today" ft. Boogie & Allan Kingdom',
+          link: 'http://www.theneedledrop.com/articles/2016/7/denzel-curry-today-ft-boogie-allan-kingdom',
+          track: 'Denzel Curry',
+        },
+        {
+          artist: '"Superstar"',
+          link: 'http://www.theneedledrop.com/articles/2016/7/a-g-cook-superstar',
+          track: 'A. G. Cook',
+        },
+        {
+          artist: '"Randeer"',
+          link: 'http://www.theneedledrop.com/articles/2016/7/dabbla-randeer',
+          track: 'Dabbla',
+        },
+        {
+          artist: '"Purple Lamborghini"',
+          link: 'http://www.theneedledrop.com/articles/2016/7/skrillex-rick-ross-purple-lamborghini',
+          track: 'Skrillex & Rick Ross',
+        },
+        {
+          artist: '"And I Grew into Ribbons"',
+          link: 'http://www.theneedledrop.com/articles/2016/7/street-sects-and-i-grew-into-ribbons',
+          track: 'Street Sects',
+        },
+        {
+          artist: '“K33p Ur Dr34ms” (Suicide Remix)',
+          link: 'http://www.theneedledrop.com/articles/2016/7/dj-windows-98-win-butler-k33p-ur-dr34ms-suicide-remix',
+          track: 'DJ Windows 98 (Win Butler)',
+        },
       ])
     })
 
--- src/tasks/extractBestTracks.ts
+++ src/tasks/extractBestTracks.ts
@@ -4,6 +4,12 @@ import { TRACKLISTS_JSON_DIR, PLAYLIST_ITEMS_JSON_PATH } from '../constants'
 
 // TODO: refactor - don't just push logic down
 
+export type BestTrack = {
+  track: string
+  artist: string
+  link: string
+}
+
 const BEST_TRACK_PREFIXES = [
   '!!!BEST TRACK',
   '!!BEST TRACK',
@@ -40,7 +46,7 @@ export default function () {
     }
 
     return acc
-  }, new Map<string, string[]>())
+  }, new Map<string, BestTrack[]>())
 
   bestTracksByYear.forEach((trackList, year) => {
     console.log(' > write', trackList.length, 'tracks for', year)
@@ -51,7 +57,7 @@ export default function () {
   })
 }
 export const extractTrackList_v2 = (item: PlaylistItem) => {
-  const trackList: string[] = []
+  const trackList: BestTrack[] = []
   // early return cases
   if (item.snippet.channelId !== item.snippet.videoOwnerChannelId) {
     return trackList
@@ -109,7 +115,7 @@ export const extractTrackList_fallback = (
   item: PlaylistItem,
   lines: string[]
 ) => {
-  const trackList: string[] = []
+  const trackList: BestTrack[] = []
   const oldPrefix = OLD_TITLE_PREFIXES.find((p) =>
     item.snippet.title.startsWith(p)
   )
@@ -139,94 +145,110 @@ export const getBestTrackStr = (line: string) => {
     return null
   }
 
-  const [bestTrack, trackLink] = lineSplit
+  const [bestTrack, link] = lineSplit
 
   if (!bestTrack.includes(' - ')) {
     return null
   }
 
-  if (!trackLink.startsWith('http')) {
+  const [track, artist] = bestTrack.split(' - ')
+
+  if (!link.startsWith('http')) {
     return null
   }
 
-  return bestTrack
+  return {
+    track,
+    artist,
+    link,
+  }
 }
 
 export const descriptionToLines = (description: string) => {
-  return description
-    .replace(/–/g, '-')
-    .replace(/\n \n/g, '\n\n')
-    .replace('Brain Tentacles -"The Sadist"', 'Brain Tentacles - "The Sadist"')
-    .replace('SORRY NOT SORRY\nDogtooth: https', 'SORRY NOT SORRY\nhttps')
-    .replace('Model/Actriz- Damocles', 'Model/Actriz - Damocles')
-    .replace(
-      `Bonnie Prince Billy \"I've Made Up My Mind`,
-      `Bonnie Prince Billy - \"I've Made Up My Mind`
-    )
-    .replace('ft. Charlie Wilson\nReview: https', 'ft. Charlie Wilson\nhttps')
-    .replace(
-      'Björk / Fever Ray co-remixes:\nhttp://bjork.lnk.to/fctheknife \nhttp://bjork.lnk.to/fcfeverray \nhttp://bjork.lnk.to/bjorkremix',
-      [
+  return (
+    description
+      .replace(/–/g, '-')
+      .replace(/\n \n/g, '\n\n')
+      .replace(
+        'Brain Tentacles -"The Sadist"',
+        'Brain Tentacles - "The Sadist"'
+      )
+      .replace('SORRY NOT SORRY\nDogtooth: https', 'SORRY NOT SORRY\nhttps')
+      .replace('Model/Actriz- Damocles', 'Model/Actriz - Damocles')
+      .replace(
+        `Bonnie Prince Billy \"I've Made Up My Mind`,
+        `Bonnie Prince Billy - \"I've Made Up My Mind`
+      )
+      .replace('ft. Charlie Wilson\nReview: https', 'ft. Charlie Wilson\nhttps')
+      // multi song
+      .replace(
+        'Björk / Fever Ray co-remixes:\nhttp://bjork.lnk.to/fctheknife \nhttp://bjork.lnk.to/fcfeverray \nhttp://bjork.lnk.to/bjorkremix',
         [
-          'Björk - Features Creatures (The Knife Remix)',
-          'http://bjork.lnk.to/fctheknife',
-        ],
+          [
+            'Björk - Features Creatures (The Knife Remix)',
+            'http://bjork.lnk.to/fctheknife',
+          ],
+          [
+            'Björk - Features Creatures (Fever Ray Remix)',
+            'http://bjork.lnk.to/fcfeverray',
+          ],
+          [
+            'Fever Ray - This Country Makes It Hard To Fuck (Björk Remix)',
+            'https://bjork.lnk.to/bjorkremix',
+          ],
+        ]
+          .map((p) => p.join('\n'))
+          .join('\n\n')
+      )
+      // multi song
+      .replace(
+        'Nails / Full of Hell split:\nhttp://www.theneedledrop.com/articles/2016/11/nails-full-of-hell-split-7',
         [
-          'Björk - Features Creatures (Fever Ray Remix)',
-          'http://bjork.lnk.to/fcfeverray',
-        ],
+          [
+            'Nails / Full of Hell - No Longer Under Your Control',
+            'https://open.spotify.com/track/6KIoYx5Xee7TyxNEOtl29k?si=deced926fa6c48e2',
+          ],
+          [
+            'Nails / Full of Hell - Thy Radiant Garotte Exposed',
+            'https://open.spotify.com/track/3XTUzlzeuaLlT7ae17pF70?si=1a0f39504b4c47b3',
+          ],
+          [
+            'Nails / Full of Hell - Bez Bólu',
+            'https://open.spotify.com/track/4WS4lLtwZQ6Ezen9Q0oFqD?si=53b1ce902cd94bc7',
+          ],
+        ]
+          .map((p) => p.join('\n'))
+          .join('\n\n')
+      )
+      // multi song
+      .replace(
+        'Disclosure - Where You Come From (Extended Mix) / Love Can Be So Hard / Where Angels Fear To Tread / Moonlight\nhttps://youtu.be/wslO7YNg3S0\nhttps://youtu.be/4CCfYi1u8Y4\nhttps://youtu.be/stixXyfsJfE\nhttps://youtu.be/yTF7LwR9YEc',
         [
-          'Fever Ray - This Country Makes It Hard To Fuck (Björk Remix)',
-          'https://bjork.lnk.to/bjorkremix',
-        ],
-      ]
-        .map((p) => p.join('\n'))
-        .join('\n\n')
-    )
-    .replace(
-      'Nails / Full of Hell split:\nhttp://www.theneedledrop.com/articles/2016/11/nails-full-of-hell-split-7',
-      [
-        [
-          'Nails / Full of Hell - No Longer Under Your Control',
-          'https://open.spotify.com/track/6KIoYx5Xee7TyxNEOtl29k?si=deced926fa6c48e2',
-        ],
-        [
-          'Nails / Full of Hell - Thy Radiant Garotte Exposed',
-          'https://open.spotify.com/track/3XTUzlzeuaLlT7ae17pF70?si=1a0f39504b4c47b3',
-        ],
-        [
-          'Nails / Full of Hell - Bez Bólu',
-          'https://open.spotify.com/track/4WS4lLtwZQ6Ezen9Q0oFqD?si=53b1ce902cd94bc7',
-        ],
-      ]
-        .map((p) => p.join('\n'))
-        .join('\n\n')
-    )
-    .replace(
-      'Disclosure - Where You Come From (Extended Mix) / Love Can Be So Hard / Where Angels Fear To Tread / Moonlight\nhttps://youtu.be/wslO7YNg3S0\nhttps://youtu.be/4CCfYi1u8Y4\nhttps://youtu.be/stixXyfsJfE\nhttps://youtu.be/yTF7LwR9YEc',
-      [
-        [
-          'Disclosure - Where You Come From (Extended Mix)',
-          'https://youtu.be/wslO7YNg3S0',
-        ],
-        ['Disclosure - Love Can Be So Hard', 'https://youtu.be/4CCfYi1u8Y4'],
-        [
-          'Disclosure - Where Angels Fear To Tread',
-          'https://youtu.be/stixXyfsJfE',
-        ],
-        ['Disclosure - Moonlight', 'https://youtu.be/yTF7LwR9YEc'],
-      ]
-        .map((p) => p.join('\n'))
-        .join('\n\n')
-    )
-    .replace(
-      'JP Moregun Mixtape\nhttp://www.jpmoregun.com/',
-      'JP Moregun - Street Signs\nhttps://open.spotify.com/track/1sB9RQHc2ZGFwUNC00YirL?si=74f1c85a180d46b4'
-    )
-    .replace(
-      'Aesop Rock / Homeboy Sandman EP\nhttp://www.theneedledrop.com/articles/2016/10/aesop-rock-homeboy-sandman-lice-two-still-buggin',
-      'Lice, Aesop Rock, Homeboy Sandman - Oatmeal Cookies\nhttps://open.spotify.com/track/17AAfKkchZ7GTBH48ODdoF?si=abe23d9591f94927'
-    )
-    .split('\n\n')
-    .map((l) => l.trim())
+          [
+            'Disclosure - Where You Come From (Extended Mix)',
+            'https://youtu.be/wslO7YNg3S0',
+          ],
+          ['Disclosure - Love Can Be So Hard', 'https://youtu.be/4CCfYi1u8Y4'],
+          [
+            'Disclosure - Where Angels Fear To Tread',
+            'https://youtu.be/stixXyfsJfE',
+          ],
+          ['Disclosure - Moonlight', 'https://youtu.be/yTF7LwR9YEc'],
+        ]
+          .map((p) => p.join('\n'))
+          .join('\n\n')
+      )
+      // multi song - just chose one for simplicity
+      .replace(
+        'JP Moregun Mixtape\nhttp://www.jpmoregun.com/',
+        'JP Moregun - Street Signs\nhttps://open.spotify.com/track/1sB9RQHc2ZGFwUNC00YirL?si=74f1c85a180d46b4'
+      )
+      // multi song - just chose one for simplicity
+      .replace(
+        'Aesop Rock / Homeboy Sandman EP\nhttp://www.theneedledrop.com/articles/2016/10/aesop-rock-homeboy-sandman-lice-two-still-buggin',
+        'Lice, Aesop Rock, Homeboy Sandman - Oatmeal Cookies\nhttps://open.spotify.com/track/17AAfKkchZ7GTBH48ODdoF?si=abe23d9591f94927'
+      )
+      .split('\n\n')
+      .map((l) => l.trim())
+  )
 }
