7bc990b3fba980e011a1988fb1512291971c0461
Joe van Bohemen <joevanbo@protonmail.com>
2023-10-27 15:08:00 +0100

    uhoh(getSpotifyItems): try assign by mutate, encountered jest issue

--- src/__tests__/getSpotifyTracks.spec.ts
+++ src/__tests__/getSpotifyTracks.spec.ts
@@ -1,307 +1,141 @@
 import * as spotifyApi from '../spotifyApi'
-import { YoutubeTrack } from '../tasks/extractYoutubeTracks'
-import {
-  YoutubeTrackWithId,
-  allocateTracks,
-  handleBatchGet,
-  handleSingleSearch,
-} from '../tasks/getSpotifyTracks'
+import { PrePlaylistItem, tryAssignByGet } from '../tasks/getSpotifyTracks'
 
 describe('getSpotifyTracks.ts', () => {
-  describe('#allocateTracks', () => {
-    it('separates tracks correctly based on track.link', () => {
-      const input = [
-        {
-          link: 'https://open.spotify.com/album/4EdIYV0iL5DFpA9V4c7pwO',
-        },
-        {
-          link: 'https://open.spotify.com/track/4m08vFKrKbjEklzRIBwllU',
-        },
-        {
-          link: 'https://youtu.be/7iS0fawNZZ0',
-        },
-        {
-          link: 'invalid url',
-        },
-      ] as YoutubeTrack[]
-
-      const { toGet, toSearch } = allocateTracks(input)
-
-      expect(toGet.length).toBe(1)
-      expect(toSearch.length).toBe(3)
-    })
-  })
-
-  describe('#handleBatchGet', () => {
+  describe('#tryAssignByGet', () => {
     const getTracksSpy = jest.spyOn(spotifyApi, 'getTracks')
     const saveFn = jest.fn()
 
-    it('combines youtube and spotify tracks by trackId', async () => {
-      const youtubeTracks = [
-        {
-          trackId: 'id_0',
-        },
-      ] as YoutubeTrackWithId[]
+    it('tries to add spotifyTracks by trackId', async () => {
+      const spotifyTracks = Array(10)
+        .fill(0)
+        .map((_, i) => ({
+          id: `id_${i}`,
+          href: `href_${i}`,
+          name: `name_${i}`,
+          artists: [
+            {
+              name: `artist_${i}`,
+            },
+          ] as spotifyApi.SpotifyArtist[],
+          uri: `spotify:track:id_${i}`,
+        }))
 
-      const spotifyTracks = youtubeTracks.map((t, i) => ({
-        id: t.trackId,
-        href: '',
-        name: `name_${i}`,
-        artists: [
-          {
-            name: `artist_${i}`,
+      const items = Array(20)
+        .fill(0)
+        .map((_, i) => ({
+          idx: i,
+          spotifyId: spotifyTracks[i] ? spotifyTracks[i].id : undefined,
+          spotifyTrack: undefined,
+          youtubeTrack: {
+            name: `name_${i}`,
+            artist: `artist_${i}`,
+            link: `link_${i}`,
           },
-        ] as spotifyApi.SpotifyArtist[],
-        uri: `spotify:track:${t.trackId}`,
-      }))
-
-      const combined = youtubeTracks.map((yt, i) => ({
-        youtubeTrack: yt,
-        spotifyTrack: {
-          id: spotifyTracks[i].id,
-          name: spotifyTracks[i].name,
-          uri: spotifyTracks[i].uri,
-          artists: spotifyTracks[i].artists.map((a) => a.name),
-        },
-      }))
+        })) as PrePlaylistItem[]
 
       getTracksSpy.mockResolvedValueOnce({
         tracks: spotifyTracks,
       })
 
-      await handleBatchGet(youtubeTracks, saveFn)
+      await tryAssignByGet(items, saveFn)
 
       expect(getTracksSpy).toBeCalledTimes(1)
-      expect(getTracksSpy).toBeCalledWith(youtubeTracks.map((t) => t.trackId))
+      expect(getTracksSpy).toHaveBeenNthCalledWith(
+        1,
+        spotifyTracks.map((i) => i.id)
+      )
       expect(saveFn).toBeCalledTimes(1)
-      expect(saveFn).toBeCalledWith(combined)
+      const firstCall = items.map((i, idx) => ({
+        ...i,
+        spotifyTrack: spotifyTracks[idx]
+          ? {
+              id: spotifyTracks[idx].id,
+              uri: spotifyTracks[idx].uri,
+              name: spotifyTracks[idx].name,
+              artists: spotifyTracks[idx].artists.map((a) => a.name),
+            }
+          : undefined,
+      }))
+      expect(saveFn).toHaveBeenNthCalledWith(1, firstCall)
     })
 
-    it('combines in batches of 50', async () => {
-      const youtubeTracks = Array(55)
+    it('adds in batches of 50', async () => {
+      const spotifyTracks = Array(55)
         .fill(0)
         .map((_, i) => ({
-          trackId: `id_${i}`,
-        })) as YoutubeTrackWithId[]
+          id: `id_${i}`,
+          href: `href_${i}`,
+          name: `name_${i}`,
+          artists: [
+            {
+              name: `artist_${i}`,
+            },
+          ] as spotifyApi.SpotifyArtist[],
+          uri: `spotify:track:id_${i}`,
+        }))
 
-      const spotifyTracks = youtubeTracks.map((t, i) => ({
-        id: t.trackId,
-        href: `href_${i}`,
-        name: `name_${i}`,
-        artists: [
-          {
-            name: `artist_${i}`,
+      const items = Array(60)
+        .fill(0)
+        .map((_, i) => ({
+          idx: i,
+          spotifyId: spotifyTracks[i] ? spotifyTracks[i].id : undefined,
+          spotifyTrack: undefined,
+          youtubeTrack: {
+            name: `name_${i}`,
+            artist: `artist_${i}`,
+            link: `link_${i}`,
           },
-        ] as spotifyApi.SpotifyArtist[],
-        uri: `spotify:track:${t.trackId}`,
-      }))
+        })) as PrePlaylistItem[]
 
-      const combined = youtubeTracks.map((yt, i) => ({
-        youtubeTrack: yt,
-        spotifyTrack: {
-          id: spotifyTracks[i].id,
-          name: spotifyTracks[i].name,
-          uri: spotifyTracks[i].uri,
-          artists: spotifyTracks[i].artists.map((a) => a.name),
-        },
-      }))
+      getTracksSpy
+        .mockResolvedValueOnce({
+          tracks: spotifyTracks.slice(0, 50),
+        })
+        .mockResolvedValueOnce({
+          tracks: spotifyTracks.slice(50),
+        })
 
-      getTracksSpy.mockResolvedValueOnce({
-        tracks: spotifyTracks.slice(0, 50),
-      })
-      getTracksSpy.mockResolvedValueOnce({
-        tracks: spotifyTracks.slice(50),
-      })
-
-      await handleBatchGet(youtubeTracks, saveFn)
+      await tryAssignByGet(items, saveFn)
 
       expect(getTracksSpy).toBeCalledTimes(2)
       expect(getTracksSpy).toHaveBeenNthCalledWith(
         1,
-        youtubeTracks.slice(0, 50).map((t) => t.trackId)
+        spotifyTracks.slice(0, 50).map((i) => i.id)
       )
       expect(getTracksSpy).toHaveBeenNthCalledWith(
         2,
-        youtubeTracks.slice(50).map((t) => t.trackId)
+        spotifyTracks.slice(50).map((i) => i.id)
       )
       expect(saveFn).toBeCalledTimes(2)
-      expect(saveFn).toHaveBeenNthCalledWith(1, combined.slice(0, 50))
-      expect(saveFn).toHaveBeenNthCalledWith(2, combined.slice(50))
-    })
-
-    it('returns items with spotifyTrack undefined if not found', async () => {
-      const youtubeTracks = Array(5)
-        .fill(0)
-        .map((_, i) => ({
-          trackId: `id_${i}`,
-        })) as YoutubeTrackWithId[]
-
-      const spotifyTracks = youtubeTracks
-        .map((t, i) => ({
-          id: t.trackId,
-          href: `href_${i}`,
-          name: `name_${i}`,
-          artists: [
-            {
-              name: `artist_${i}`,
-            },
-          ] as spotifyApi.SpotifyArtist[],
-          uri: `spotify:track:${t.trackId}`,
-        }))
-        .slice(0, 3)
-
-      const combined = youtubeTracks.map((yt, i) => ({
-        youtubeTrack: yt,
-        spotifyTrack: spotifyTracks[i]
+      // const firstCall = items.map((i, idx) => ({
+      //   ...i,
+      //   spotifyTrack:
+      //     idx < 50
+      //       ? {
+      //           id: spotifyTracks[idx].id,
+      //           uri: spotifyTracks[idx].uri,
+      //           name: spotifyTracks[idx].name,
+      //           artists: spotifyTracks[idx].artists.map((a) => a.name),
+      //         }
+      //       : undefined,
+      // }))
+      // currently both calls are the same!
+      // https://github.com/jestjs/jest/issues/7950
+      // expect(saveFn.mock.calls[0][0]).toEqual(saveFn.mock.calls[1][0])
+      // expect(saveFn).toHaveBeenNthCalledWith(1, firstCall)
+      const secondCall = items.map((i, idx) => ({
+        ...i,
+        spotifyTrack: spotifyTracks[idx]
           ? {
-              id: spotifyTracks[i].id,
-              name: spotifyTracks[i].name,
-              uri: spotifyTracks[i].uri,
-              artists: spotifyTracks[i].artists.map((a) => a.name),
+              id: spotifyTracks[idx].id,
+              uri: spotifyTracks[idx].uri,
+              name: spotifyTracks[idx].name,
+              artists: spotifyTracks[idx].artists.map((a) => a.name),
             }
           : undefined,
       }))
-
-      getTracksSpy.mockResolvedValueOnce({
-        tracks: spotifyTracks,
-      })
-
-      await handleBatchGet(youtubeTracks, saveFn)
-
-      expect(getTracksSpy).toBeCalledTimes(1)
-      expect(getTracksSpy).toHaveBeenNthCalledWith(
-        1,
-        youtubeTracks.map((t) => t.trackId)
-      )
-      expect(saveFn).toBeCalledTimes(1)
-      expect(saveFn).toHaveBeenNthCalledWith(1, combined)
-    })
-  })
-
-  describe('#handleSingleSearch', () => {
-    const findTrackSpy = jest.spyOn(spotifyApi, 'findTrack')
-    const saveFn = jest.fn()
-
-    it('combines youtube and spotify tracks by trackId', async () => {
-      const year = 2020
-      const youtubeTracks = Array(5)
-        .fill(0)
-        .map((_, i) => ({
-          trackId: `id_${i}`,
-        })) as YoutubeTrackWithId[]
-
-      const spotifyTracks = youtubeTracks.map((t, i) => ({
-        id: t.trackId,
-        href: `href_${i}`,
-        name: `name_${i}`,
-        artists: [
-          {
-            name: `artist_${i}`,
-          },
-        ] as spotifyApi.SpotifyArtist[],
-        uri: `spotify:track:${t.trackId}`,
-      }))
-
-      const combined = youtubeTracks.map((yt, i) => ({
-        youtubeTrack: yt,
-        spotifyTrack: {
-          id: spotifyTracks[i].id,
-          name: spotifyTracks[i].name,
-          uri: spotifyTracks[i].uri,
-          artists: spotifyTracks[i].artists.map((a) => a.name),
-        },
-      }))
-
-      youtubeTracks.forEach((_, i) => {
-        const t = spotifyTracks[i]
-        findTrackSpy.mockResolvedValueOnce({
-          tracks: {
-            href: '',
-            items: [
-              {
-                href: '',
-                id: t.id,
-                name: t.name,
-                uri: t.uri,
-                artists: t.artists,
-              },
-            ],
-          },
-        })
-      })
-
-      await handleSingleSearch(youtubeTracks, year, saveFn)
-
-      expect(findTrackSpy).toBeCalledTimes(5)
-      expect(findTrackSpy.mock.calls).toEqual(
-        youtubeTracks.map((t) => [t, year])
-      )
-      expect(saveFn).toBeCalledTimes(5)
-      expect(saveFn.mock.calls).toEqual(combined.map((c) => [[c]]))
-    })
-
-    it('returns items with spotifyTrack undefined if not found', async () => {
-      const year = 2020
-      const youtubeTracks = Array(5)
-        .fill(0)
-        .map((_, i) => ({
-          trackId: `id_${i}`,
-        })) as YoutubeTrackWithId[]
-
-      const spotifyTracks = youtubeTracks
-        .map((t, i) => ({
-          id: t.trackId,
-          href: `href_${i}`,
-          name: `name_${i}`,
-          artists: [
-            {
-              name: `artist_${i}`,
-            },
-          ] as spotifyApi.SpotifyArtist[],
-          uri: `spotify:track:${t.trackId}`,
-        }))
-        .slice(0, 3)
-
-      const combined = youtubeTracks.map((yt, i) => ({
-        youtubeTrack: yt,
-        spotifyTrack: spotifyTracks[i]
-          ? {
-              id: spotifyTracks[i].id,
-              name: spotifyTracks[i].name,
-              uri: spotifyTracks[i].uri,
-              artists: spotifyTracks[i].artists.map((a) => a.name),
-            }
-          : undefined,
-      }))
-
-      youtubeTracks.forEach((_, i) => {
-        const t = spotifyTracks[i]
-        findTrackSpy.mockResolvedValueOnce({
-          tracks: {
-            href: '',
-            items: t
-              ? [
-                  {
-                    href: '',
-                    id: t.id,
-                    name: t.name,
-                    uri: t.uri,
-                    artists: t.artists,
-                  },
-                ]
-              : [],
-          },
-        })
-      })
-
-      await handleSingleSearch(youtubeTracks, year, saveFn)
-
-      expect(findTrackSpy).toBeCalledTimes(5)
-      expect(findTrackSpy.mock.calls).toEqual(
-        youtubeTracks.map((t) => [t, year])
-      )
-      expect(saveFn).toBeCalledTimes(5)
-      expect(saveFn.mock.calls).toEqual(combined.map((c) => [[c]]))
+      expect(saveFn).toHaveBeenNthCalledWith(2, secondCall)
     })
   })
 })
--- src/tasks/getSpotifyTracks.ts
+++ src/tasks/getSpotifyTracks.ts
@@ -1,4 +1,4 @@
-import { read, readFileSync, readdirSync, writeFileSync } from 'fs'
+import { readFileSync, readdirSync, writeFileSync } from 'fs'
 import { PLAYLIST_PREP_JSON_DIR, YOUTUBE_TRACKS_JSON_DIR } from '../constants'
 import { YoutubeTrack as YoutubeTrack } from './extractYoutubeTracks'
 import {
@@ -8,18 +8,19 @@ import {
   getTracks,
   setToken,
 } from '../spotifyApi'
-import { fileHelper } from '../fileApi'
 
-export type YoutubeTrackWithId = YoutubeTrack & {
-  trackId: string
-}
 export type TrimSpotifyTrack = Pick<SpotifyTrack, 'id' | 'uri' | 'name'> & {
   artists: string[]
 }
 
 export type PrePlaylistItem = {
+  idx: number
+  spotifyId: string | null
   youtubeTrack: YoutubeTrack
   spotifyTrack?: TrimSpotifyTrack
+}
+export type WithSpotifyId = PrePlaylistItem & {
+  spotifyId: string
 }
 
 export default async function () {
@@ -32,11 +33,6 @@ export default async function () {
 
   let fileNum = 1
   for (const jsonFile of bestTrackFiles) {
-    const year = parseInt(jsonFile.replace('.json', ''))
-    const { saveFn } = fileHelper<PrePlaylistItem>(
-      `${PLAYLIST_PREP_JSON_DIR}/${jsonFile}`
-    )
-
     const tracksFromFile: YoutubeTrack[] = JSON.parse(
       readFileSync(`${YOUTUBE_TRACKS_JSON_DIR}/${jsonFile}`, 'utf8')
     )
@@ -51,94 +47,86 @@ export default async function () {
       'tracks'
     )
 
-    const { toGet, toSearch } = allocateTracks(tracksFromFile)
-    console.log('  > toGet:', toGet.length, ' toSearch:', toSearch.length)
+    const year = parseInt(jsonFile.replace('.json', ''))
+    const saveFn = (d: PrePlaylistItem[]) =>
+      writeFileSync(
+        `${PLAYLIST_PREP_JSON_DIR}/${jsonFile}`,
+        JSON.stringify(d, null, 2)
+      )
 
-    await handleBatchGet(toGet, saveFn)
-
-    await handleSingleSearch(toSearch, year, saveFn)
-  }
-}
-
-export const allocateTracks = (tracks: YoutubeTrack[]) => {
-  const toGet: YoutubeTrackWithId[] = []
-  const toSearch: YoutubeTrack[] = []
-
-  tracks.forEach((track) => {
-    const trackId = extractSpotifyId(track.link, 'track')
-    if (trackId) {
-      toGet.push({ ...track, trackId })
-    } else {
-      toSearch.push(track)
-    }
-  })
-
-  return {
-    toGet,
-    toSearch,
-  }
-}
-
-export const handleBatchGet = async (
-  tracks: YoutubeTrackWithId[],
-  saveFn: (nextItems: PrePlaylistItem[]) => void
-): Promise<void> => {
-  for (let i = 0; i < tracks.length; i += 50) {
-    const batch = tracks.slice(i, i + 50)
-
-    const result = await getTracks(batch.map((t) => t.trackId))
-
-    const spotifyMap = new Map<string, TrimSpotifyTrack>()
-    result.tracks.forEach((t) => {
-      spotifyMap.set(t.id, {
-        id: t.id,
-        name: t.name,
-        artists: t.artists.map((a) => a.name),
-        uri: t.uri,
-      })
-    })
-
-    const nextItems = batch.map((t) => ({
+    const items = tracksFromFile.map((t, i) => ({
+      idx: i,
+      spotifyId: extractSpotifyId(t.link, 'track'),
       youtubeTrack: t,
-      spotifyTrack: spotifyMap.get(t.trackId),
+      spotifyTrack: undefined,
     }))
 
-    saveFn(nextItems)
+    await tryAssignByGet(items, saveFn)
+
+    await tryAssignBySearch(items, year, saveFn)
   }
 }
 
-export const handleSingleSearch = async (
-  tracks: YoutubeTrack[],
+export const tryAssignByGet = async (
+  items: PrePlaylistItem[],
+  saveFn: (d: PrePlaylistItem[]) => void
+) => {
+  // https://stackoverflow.com/questions/43118692/typescript-filter-out-nulls-from-an-array
+  const withSpotifyIds: WithSpotifyId[] = items.filter(
+    (i): i is WithSpotifyId => !!i.spotifyId
+  )
+
+  for (let i = 0; i < withSpotifyIds.length; i += 50) {
+    const batch = withSpotifyIds.slice(i, i + 50)
+
+    const result = await getTracks(batch.map((i) => i.spotifyId))
+
+    const spotifyTrackMap = new Map<string, TrimSpotifyTrack>()
+    result.tracks.forEach((t) =>
+      spotifyTrackMap.set(t.id, {
+        id: t.id,
+        uri: t.uri,
+        name: t.name,
+        artists: t.artists.map((a) => a.name),
+      })
+    )
+
+    const _items = items.slice(0)
+    batch.forEach((i) => {
+      _items[i.idx].spotifyTrack = spotifyTrackMap.get(i.spotifyId)
+    })
+
+    saveFn(_items)
+  }
+}
+
+export const tryAssignBySearch = async (
+  items: PrePlaylistItem[],
   year: number,
-  saveFn: (nextItems: PrePlaylistItem[]) => void
+  saveFn: (d: PrePlaylistItem[]) => void
 ): Promise<void> => {
   let trackNum = 1
-  for (const youtubeTrack of tracks) {
-    const logMsg = `  > ${trackNum++} / ${tracks.length}`
+  for (const item of items) {
+    const logMsg = `  > ${trackNum++} / ${items.length}`
     process.stdout.write(`${logMsg}\r`)
 
     let ts1 = Date.now()
     // await new Promise((r) => setTimeout(r, 10 * Math.random() + 10))
-    const results = await findTrack(youtubeTrack, year)
+    const results = await findTrack(item.youtubeTrack, year)
     let ts2 = Date.now()
     const elapsedMS = Math.round(ts2 - ts1)
     process.stdout.write(`${logMsg} ${elapsedMS}ms\r`)
 
-    const nextItem: PrePlaylistItem = {
-      youtubeTrack,
-      spotifyTrack: undefined,
-    }
-
     if (results.tracks.items.length) {
       const { id, uri, artists, name } = results.tracks.items[0]
-      nextItem.spotifyTrack = {
+      items[item.idx].spotifyTrack = {
         id,
         uri,
         name,
         artists: artists.map((a) => a.name),
       }
-    }
 
-    saveFn([nextItem])
+      saveFn(items)
+    }
   }
 }
