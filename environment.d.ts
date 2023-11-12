32788f23be5be3de7d6cad9ca5d5928caeeab044
Joe van Bohemen <joevanbo@protonmail.com>
2023-11-12 21:32:38 +0000

    add google sheets api methods

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      SPREADSHEET_ID: string
+      GOOGLE_PRIVATE_KEY: string
       YOUTUBE_API_KEY: string
       SPOTIFY_CLIENT_ID: string
       SPOTIFY_CLIENT_SECRET: string
---
+++ src/sheetsApi.ts
@@ -0,0 +1,69 @@
+import { google, sheets_v4 } from 'googleapis'
+
+// https://github.com/JosephJvB/gsheets-api/blob/main/src/database/sheetClient.ts
+
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+let _client: sheets_v4.Sheets | undefined
+
+export const getClient = () => {
+  if (!_client) {
+    // https://stackoverflow.com/questions/30400341/environment-variables-containing-newlines-in-node
+    const privateKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    const authClient = new google.auth.GoogleAuth({
+      scopes: SCOPES,
+      credentials: {
+        private_key: privateKey,
+        client_email: process.env.GOOGLE_ACCOUNT_client_email,
+      },
+    })
+    _client = google.sheets({
+      version: 'v4',
+      auth: authClient,
+    })
+  }
+  return _client
+}
+
+export const createSheet = async (sheetName: string) => {
+  await getClient().spreadsheets.batchUpdate({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    requestBody: {
+      requests: [
+        {
+          addSheet: {
+            properties: {
+              title: sheetName,
+            },
+          },
+        },
+      ],
+    },
+  })
+}
+
+export const getRows = async (sheetName: string, range: string) => {
+  const res = await getClient().spreadsheets.values.get({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: `${sheetName}!${range}`,
+  })
+  return res.data.values || []
+}
+
+export const addRow = async (
+  sheetName: string,
+  range: string,
+  row: string[]
+) => {
+  await getClient().spreadsheets.values.append({
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: `${sheetName}!${range}`,
+    valueInputOption: 'RAW',
+    insertDataOption: 'INSERT_ROWS',
+    includeValuesInResponse: true,
+    requestBody: {
+      majorDimension: 'ROWS',
+      values: [row],
+    },
+  })
+}
